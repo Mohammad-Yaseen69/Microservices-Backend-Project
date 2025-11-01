a64acd03ba6f826574b8802af5cdd90f09a0aa22
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2025-11-01 18:00:18 +0500

    add  - eslint & code formating

---
+++ user-service/src/constant.js
@@ -0,0 +1,4 @@
+export const cookiesSettings = {
+    httpsOnly: true,
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+}
--- user-service/src/server.js
+++ user-service/src/server.js
@@ -1,16 +1,8 @@
-import cors from "cors"
 import dotenv from "dotenv"
 import app from "./app.js"
-import express from "express"
 import { connectDb } from "./db/index.js"
 
 dotenv.config()
-
-app.use(express.json())
-app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-}))
 
 const PORT = process.env.PORT || 3001
 
