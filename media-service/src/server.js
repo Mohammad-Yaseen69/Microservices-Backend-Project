81da85978fb85ec506f29f72e4093ba73c962f0e
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2025-11-03 23:38:50 +0500

    add - setup for media service

---
+++ media-service/src/server.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv"
+import app from "./app.js"
+import { connectDb } from "./db/index.js"
+
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3001
+
+const startServer = async () => {
+    await connectDb()
+    app.listen(PORT, () => {
+        console.log(`Post service running on port ${PORT}`)
+    })
+}
+
+
+startServer()
